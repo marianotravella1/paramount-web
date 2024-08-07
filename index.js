fca2d75b1913ccb44543be3c3a09f376ddb5af6f
Mariano <marianotravella2@gmail.com>
2024-08-07 19:52:24 -0300

    mejora diseño + favicon + intro a js

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hola TUP");
+
+let nombre = "Mariano";
+
+let edad = 21;
+
+let esprofe = false;
+
+let nombreprofes = [nombre, "Juan", "Miguel"];
+
+let nada = null;
+let vacio = undefined;
+
+let profe = {
+  nombre: nombre,
+  edad: edad,
+  esprofe: esprofe,
+};
+
+console.log(nombre);
+console.log(edad);
+console.log(esprofe);
+console.log(nombreprofes[nombreprofes.length-1]);
